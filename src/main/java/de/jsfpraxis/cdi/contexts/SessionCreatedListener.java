38414b9c56aa91f9cb94ea44067fc51ac7f6ca0c
Bernd Mueller <bernd.mueller@ostfalia.de>
2020-05-19 20:36:27 +0200

    Event listener for session destroyed

--- src/main/java/de/jsfpraxis/cdi/contexts/SessionCreatedListener.java
+++ src/main/java/de/jsfpraxis/cdi/contexts/SessionCreatedListener.java
@@ -2,6 +2,7 @@ package de.jsfpraxis.cdi.contexts;
 
 import java.util.logging.Logger;
 
+import javax.enterprise.context.Destroyed;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.context.SessionScoped;
 import javax.enterprise.event.Observes;
@@ -12,6 +13,10 @@ public class SessionCreatedListener {
 	public void sessionCreated(@Observes @Initialized(SessionScoped.class) HttpSession session) {
 		Logger.getLogger(SessionCreatedListener.class.getCanonicalName()).info("Session created: " + session.getId());
 	}
-	
+
+	public void sessionRemoved(@Observes @Destroyed(SessionScoped.class) HttpSession session) {
+		Logger.getLogger(SessionCreatedListener.class.getCanonicalName()).info("Session removed: " + session.getId());
+	}
+
 }
 
